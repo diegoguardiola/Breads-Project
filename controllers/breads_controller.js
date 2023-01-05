29826bb2a6bd6eb5ed30d5befe29b00e0ec95cb8
Diego I. Guardiola <diegoguardiola01@gmail.com>
2023-01-05 18:53:02 -0500

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,12 +22,14 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
 })
+
 
 // CREATE
 breads.post('/', (req, res) => {
@@ -43,5 +45,10 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 module.exports = breads
