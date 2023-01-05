5c9d8264813c470970c701eaa40ff3a0193394fb
Diego I. Guardiola <diegoguardiola01@gmail.com>
2023-01-04 20:45:41 -0500

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -3,26 +3,21 @@ const Default = require('./layouts/Default')
 
 function Index ({breads})  {
     return (
-    <Default title={title}>
+        <Default>
         <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
-        <ul>
-        {
-        breads.map((bread, index)=> {
-            return (
-                <li key={index}>
-                    <a href={`/breads/${index}`}>
+            {/* This is a JSX comment. */}
+            {/* <p>I have {breads[0].name} bread!</p> */} 
+            <ul>
+                {breads.map((bread, index) => {
+                    return (<li key={index}>
+                        <a href = {`/breads/${index}`}>
                         {bread.name}
-                    </a>
-                </li>
-                )
-            })
-        }
-        </ul>
-    </Default>
+                        </a>  
+                    </li>)
+                })}
+            </ul>
+        </Default>
     )
 }
-
 
 module.exports = Index
