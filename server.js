206068991d43c4640ed8cbfa8fde901cdd494ef0
Diego I. Guardiola <diegoguardiola01@gmail.com>
2023-01-04 21:19:52 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 // ROUTES
@@ -31,4 +32,10 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 
