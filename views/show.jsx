e2e0309013ee787f95463d556a365a8beeb4af05
Diego I. Guardiola <diegoguardiola01@gmail.com>
2023-01-05 19:29:53 -0500

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -7,6 +7,7 @@ function Show ({bread, index}) {
       <Default>   
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type='submit' value="DELETE"/>
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
         </form>
         <h3>{bread.name}</h3>
         <p>
